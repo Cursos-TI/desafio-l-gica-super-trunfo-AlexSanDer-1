780a1b97cd9923f4627a22eeafa910fb83b0e89b
AlexSanDer-1 <odhsanderspindola@gmail.com>
2025-09-10 00:37:17 +0000

    Comparacao entre duas cartas

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -39,5 +39,132 @@ int main() {
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
+
+  // Atributos referente a carta 1 do Usuario
+   char estado;
+   char codigoCarta[50];
+   char nomeCidade[50];
+   int populacao;
+   float area;
+   float pib;
+   int pontos_turisticos;
+   float densidade_populacional;
+   float pib_capita;
+
+   // Atributos referente a carta 2 do Usuario
+   char estado_2;
+   char codigoCarta_2[50];
+   char nomeCidade_2[50];
+   int populacao_2;
+   float area_2;
+   float pib_2;
+   int pontos_turisticos_2;
+   float densidade_populacional_2;
+   float pib_capita_2;
+
+   // Interação com usuario,para cadastrar dados da carta 1
+   printf("Digite estado da carta: ");
+   scanf(" %c", &estado);
+
+   printf("Digite codigo da carta: ");
+   scanf("%s", &codigoCarta);
+   getchar(); // limpa /n deixado pelo scanf
+
+   printf("Digite nome da cidade: ");
+   fgets(nomeCidade, sizeof(nomeCidade), stdin); // Lê uma linha inteira,inclusive com espaços
+
+   printf("Digite o numero de habitantes da cidade: ");
+   scanf("%d", &populacao);
+
+   printf("Digite area  da cidade em quilômetros: ");
+   scanf("%f", &area);
+
+   printf("Digite produto interno bruto da cidade: ");
+   scanf("%f", &pib);
+
+   printf("Digite pontos turisticos: ");
+   scanf("%d", &pontos_turisticos);
+
+   // Área de calculo carta 1
+   densidade_populacional = populacao / area;
+
+   // (pib * 1000000000) converter o PIB de bilhões para unidades monetárias completas.
+
+   pib_capita = (pib * 1000000000)  / populacao;
+
+   // Interação com usuario,para cadastrar dados da carta 2
+   printf("----------------------------\n");
+
+   printf("Digite estado da carta: ");
+   scanf(" %c", &estado_2);
+
+   printf("Digite codigo da carta: ");
+   scanf("%s", &codigoCarta_2);
+   getchar(); // limpa /n deixado pelo scanf
+
+   printf("Digite nome da cidade: ");
+   fgets(nomeCidade_2, sizeof(nomeCidade_2), stdin); // Lê uma linha inteira,inclusive com espaços
+
+   printf("Digite o numero de habitantes da cidade: ");
+   scanf("%d", &populacao_2);
+
+   printf("Digite area  da cidade em quilômetros: ");
+   scanf("%f", &area_2);
+
+   printf("Digite produto interno bruto da cidade: ");
+   scanf("%f", &pib_2);
+
+   printf("Digite pontos turisticos: ");
+   scanf("%d", &pontos_turisticos_2);
+
+   // Área de calculo carta 2
+   densidade_populacional_2 = populacao_2 / area_2;
+
+
+   pib_capita_2 = (pib_2 * 1000000000) / populacao_2;
+
+
+
+   
+
+   // Leitura dos dados fornecidos da carta 1
+   printf("----------------------------\n");
+   printf("Carta 1 \n");
+   printf("Código: %c%s \n", estado, codigoCarta);
+   printf("Nome da cidade: %s", nomeCidade);
+   printf("População: %d\n", populacao);
+   printf("Área: %.2f km² \n", area);
+   printf("PIB: %.2f bilhões de reais\n", pib);
+   printf("Número de Pontos turisticos: %d \n", pontos_turisticos);
+   printf("Densidade populacional: %.2f hab/km² \n", densidade_populacional);
+   printf("PIB per capita: %.2f\n", pib_capita, "reais");
+   
+
+   // Leitura dos dados fornecidos da carta 2
+   printf("----------------------------\n");
+   printf("Carta 2 \n");
+   printf("Código: %c%s \n", estado_2, codigoCarta_2);
+   printf("Nome da cidade: %s", nomeCidade_2);
+   printf("População: %d\n", populacao_2);
+   printf("Área: %.2f km² \n", area_2);
+   printf("PIB: %.2f bilhões de reais\n", pib_2);
+   printf("Número de Pontos turisticos: %d \n", pontos_turisticos_2);
+   printf("Densidade populacional: %.2f hab/km² \n", densidade_populacional_2);
+   printf("PIB per capita: %.2f reais \n ", pib_capita_2);
+   
+
+   printf("----------------------------\n");
+   printf("       ***RESULTADO***    \n");
+
+   if (populacao > populacao_2) /*Comparação entre duas cartas é determinada pela população,
+                                 a carta é referia  pelo seu codigo e estado */
+   {
+      printf("Carta %c%s  Venceu! \n",estado,codigoCarta);
+   }else{
+      printf("Carta %c%s  Venceu! \n",estado_2,codigoCarta_2);
+   }
+ 
+   
+
     return 0;
 }
